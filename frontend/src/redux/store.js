2df2f0984b28f0d0d767d112260aab68ee5282d6
Diana-codes <dianaruzindana8@gmail.com>
2024-11-29 09:01:22 +0200

    school management system

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,31 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { userReducer } from './userRelated/userSlice';
+import { studentReducer } from './studentRelated/studentSlice';
+import { noticeReducer } from './noticeRelated/noticeSlice';
+import { sclassReducer } from './sclassRelated/sclassSlice';
+import { teacherReducer } from './teacherRelated/teacherSlice';
+import { complainReducer } from './complainRelated/complainSlice';
+
+const store = configureStore({
+    reducer: {
+        user: userReducer,
+        student: studentReducer,
+        teacher: teacherReducer,
+        notice: noticeReducer,
+        complain: complainReducer,
+        sclass: sclassReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            // Ignore these action types
+            ignoredActions: ['your/action/type'],
+            // Ignore these field paths in all actions
+            ignoredActionPaths: ['meta.arg', 'payload.timestamp'],
+            // Ignore these paths in the state
+            ignoredPaths: ['items.dates'],
+          },
+        }),
+});
+
+export default store;
